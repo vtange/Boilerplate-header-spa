7f71ca3fd753c8c5cb4dcea6265a7ec2a4b9db2b
Victor Tang <victortang1992@gmail.com>
2016-02-10 15:49:41 -0800

    add angular, add login toggle

---
+++ public/header.js
@@ -0,0 +1,18 @@
+(function() {
+    //start of function
+  var app = angular.module('header', []);
+
+app.controller('MainCtrl', ['$scope', function($scope){
+	$scope.showLogin = false;
+	$scope.toggleLogin = function(){
+		$scope.showLogin = $scope.showLogin===true?false:true;
+	}
+    $scope.login = function(){
+		
+	}
+	$scope.logout = function(){
+		
+	}
+}]);//end of controller
+  //end of function
+})();
