d39cf82341749d91d6b27794d819bfdf41a21e53
Victor Tang <victortang1992@gmail.com>
2016-02-10 19:49:13 -0800

    login form

--- public/header.js
+++ public/header.js
@@ -2,12 +2,21 @@
     //start of function
   var app = angular.module('header', []);
 
-app.controller('MainCtrl', ['$scope', function($scope){
+app.controller('MainCtrl', ['$scope', '$http', '$window', function($scope, $http, $window){
+	$scope.info = {};
 	$scope.showLogin = false;
 	$scope.toggleLogin = function(){
 		$scope.showLogin = !!($scope.showLogin)?false:true;
 	}
     $scope.login = function(){
+		console.log($scope.info);
+		$http.post($window.location.href+"login",$scope.info).success(function(data){
+			console.log(data);
+			
+		}).error(function(err){
+			throw err;
+		})
+		
 		
 	}
 	$scope.logout = function(){
